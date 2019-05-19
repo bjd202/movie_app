16a570f2c9d959e2d864b093047c5e9963315e2f
bjd202 <bjd202@naver.com>
2019-05-19 22:44:37 +0900

    Updates

---
+++ precache-manifest.edcc600850d9ac241ed85bb72cddc0ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3078a45249f7320e99606269c35acc2f",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "d7da14f02ea0ddc3063a",
+    "url": "/movie_app/static/css/main.ed3c37b9.chunk.css"
+  },
+  {
+    "revision": "ad63ab08a2a8e5504836",
+    "url": "/movie_app/static/js/2.7b834e7d.chunk.js"
+  },
+  {
+    "revision": "d7da14f02ea0ddc3063a",
+    "url": "/movie_app/static/js/main.899ffa5b.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
